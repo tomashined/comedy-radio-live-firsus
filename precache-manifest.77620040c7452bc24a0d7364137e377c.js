e543f9aa62a8419e47417060fb4c4ccfafb38930
tomashined <45771896+tomashined@users.noreply.github.com>
2020-02-19 19:27:20 +0300

    Updates

---
+++ precache-manifest.77620040c7452bc24a0d7364137e377c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c57bd73db36a780a1d1170b8e970145",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2cd669d26f2dc428522",
+    "url": "./static/css/2.35741284.chunk.css"
+  },
+  {
+    "revision": "92e8d0b0aaab8e77a248",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f2cd669d26f2dc428522",
+    "url": "./static/js/2.fb0cfba5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.fb0cfba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e8d0b0aaab8e77a248",
+    "url": "./static/js/main.e574cd37.chunk.js"
+  },
+  {
+    "revision": "c013ddcd007cea0c706c",
+    "url": "./static/js/runtime-main.8de71be9.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/comedy-radio-live-firsus/precache-manifest.c58628ef6ab3af8e54f6091972c200d2.js"
+  "./precache-manifest.77620040c7452bc24a0d7364137e377c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/comedy-radio-live-firsus/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
