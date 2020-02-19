811f2311cbb4166878f82e13e974785e9b01e4df
tomashined <45771896+tomashined@users.noreply.github.com>
2020-02-19 19:49:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-//import 'core-js/features/map';
-//import 'core-js/features/set';
+import 'core-js/features/map';
+import 'core-js/features/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vk-connect';
