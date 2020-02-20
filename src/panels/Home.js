8e958a36e96bb52b6930d17ed4331de0b02d7e98
tomashined <45771896+tomashined@users.noreply.github.com>
2020-02-20 19:32:11 +0300

    radio

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -8,10 +8,14 @@ import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 import ComedyRadioHomeSiteLogo from '../img/ComedyRadioHomeSiteLogo.svg';
+import BannerComedyRadio from '../img/BannerComedyRadio.jpg';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<img className="Persik" src={ComedyRadioHomeSiteLogo} alt="Persik The Cat"/>
+		<img className="BannerComedyRadio" src={BannerComedyRadio} alt="Persik The Cat"/>
+		<a href="https://comedy-radio.ru/" target="_blank">
+			<img className="ComedyRadioHomeSiteLogo" src={ComedyRadioHomeSiteLogo} alt="Persik The Cat"/>
+		</a>
 		<PanelHeader>Comedy Radio Live</PanelHeader>
 		
 		<Group title="Navigation Example">
@@ -22,27 +26,18 @@ const Home = ({ id, go, fetchedUser }) => (
 			</Div>
 		</Group>
 
-		<img className="Persik" src={ComedyRadioHomeSiteLogo} alt="Persik The Cat"/>
+		<video controls="" autoplay="" name="media">
+			<source src="http://ic7.101.ru:8000/a202" type="audio/aac">
+
+			</source>
+		</video>
 
 		<Group title="Navigation Example">
-				<Button size="xl" level="2" href="https://comedy-radio.ru/" target="_blank">
+				<Button size="xl" level="2" href="http://ic7.101.ru:8000/a202" target="_blank">
 					<img className="Persik" src={ComedyRadioHomeSiteLogo} alt="Persik The Cat"/>
 				</Button>
 		</Group>
 	</Panel>
 );
-
-Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
-};
 
 export default Home;
